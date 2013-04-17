9560488ad2f55513d28ae521017281df4d2da9fd
Raj Prashad <rprashad@cheetahmail.com>
2013-04-17 16:53:25 -0400

    removed some crusty stuff

--- Container/INI.pm
+++ Container/INI.pm
@@ -18,36 +18,22 @@ package Container::INI;
 use strict;
 
 sub new() {
-	my $class = shift;
-	my $config = shift;
-	my $self = {};
+	my $class = $_[0];
+	my $self = { 'file' => $_[1] };
 	bless($self,$class);
-	$self->source_config($config);
+	$self->source_config;
 	return $self;
 } 
-sub source_config($) {
-	my $self = shift;
-	my $file = shift;
-	my $cwd = `pwd`; chomp($cwd);
+sub source_config {
+	my $self = $_[0];
 
-	# check current working directory
-	if ( -e $cwd . "/" . $file) {
-		$file = $cwd . "/" . $file;
-	}
-	
-    $self->{config} = $file;
-	
-	
-        open(CFG, "< $file") or 
-		die __PACKAGE__ . "Error reading ini: $file\n";
-
+        open(CFG, "< $self->{file}" ) or die __PACKAGE__ . "Error reading ini: $self->{file}\n";
 	my ($module, $hpoint, $key, $value, $evalue, $continue, $quoted);
 	while(<CFG>) {
-		chomp;
+		chomp($_);
 		# ignore lines that begin with '#' or ';' or whitespace
-
                 if ($_ !~ /^\s*?#/ && $_ ne "" && $_ !~ /^;/ or $quoted > 0) {
-			# LOOK FOR HEADER INFO
+			# Find context/header
                		if ($_ =~ /^\s*\[\s*(\w+)?:?:?(\w+)?\s*\]/) {
 				my $mod = $1;
 				my $options = $2;
@@ -65,45 +51,48 @@ sub source_config($) {
 				}
 			}
 		
-			# OTHERWISE PROCESS KEY/VAL PAIRS
+			# process key/value pairs
 			else {
 				# line continuation code
 				if (!$continue and !$quoted) {
 					($key, $value) = split("=",$_,2);
 					$key =~ s/\s+//g;
-					# $key = $1;
 				}
 				else {
 					$value = $_;
 				}
-				
+			
+				# found line continuation char \
 				if ($value =~ /(.*)\s*?\\\s*?$/) {
-					# print "Found line continuation: $value\n";
 					$continue += 1;
 				}
+
+				# found quoted text
 				elsif ($value =~ /"/ && $value !~ /"[^"]+"/) {
 					$quoted++;
-					# $value =~ s/^\s*//g;
 				}
 
+				# single line entity - build/merge
 				if (!$continue and !$quoted) {
 					my $conf = $self->build_conf($key, $value);
 					$$hpoint = $self->merge_conf($$hpoint, $conf);
 				}
+			
+				# found line continuation char \	
 				elsif ($value =~ /(.*)?\s*?\\\s*?$/) {
 						$value = $1 . "\n";
 						$evalue .= $value;
 				}
+				# found a quote
 				elsif ($quoted == 1) {
 						$evalue .= "$value\n";
 				}
+				# we've reached the end of our multi-line value
 				else {
 						$evalue .= $value;
 						my $conf = $self->build_conf($key, $evalue);
 						$$hpoint = $self->merge_conf($$hpoint, $conf);
-						$continue = undef;
-						$evalue = undef;
-						$quoted = undef;
+						$continue = $evalue = $quoted = undef;
 				}
 			}
 		}
@@ -115,33 +104,12 @@ sub source_config($) {
 }
 
 sub get_config() {
-	my $self = shift;
-	return $self->{conf};
+	return $_[0]->{conf};
 }
 
 sub merge_conf() {
-=pod
 
-=over 4
-
-=item merge_conf (private)
-
-This doesn't actually modify the first hash unless you have references within an arrayref.
-
-B<THIS IS NOT A DEEP COPY> 
-
-I<Arguements>: hashref to be default values, hashref of data to write over in the first hashref
-
-I<Returns>: hashref of resulting data (their may be references to the first hashref)
-
-=back
-
-=cut
-
-my $self = shift;
-my $dst = shift;
-my $src = shift;
-
+my ($self, $dst, $src) = @_,3;
 my $return =  { %{ $dst || {} } }; # shallow copy (if undef will give an new empty hash
 
 foreach my $key (keys %{$src}) {
@@ -165,37 +133,24 @@ foreach my $key (keys %{$src}) {
 }
 return $return;
 
-} ################################################## END merge_conf
+} 
 
 
 sub build_conf() {
-	my $self = shift;
-	my $key = shift;
-	my $value = shift;
-	my $conf = $self->is_dot_notation($key, $value);
-	return $conf;
-}
-
-sub is_dot_notation() {
-
-	my $self = shift;
-	my $key = shift;
-	my $value = shift;
-	my @dots = split(/\./, $key);
-	return $self->recursive_hash(\@dots, $value);
+	my ($self, $key, $value) = @_,3;
+	return $self->recursive_hash([ split /\./, $key ], $value);
 }
 
 sub recursive_hash() {
-	my $self = shift;
-	my @array = @{shift(@_)};
-	my $val = shift(@_);
-	my $e = shift(@array);
-	my $hash;
-	if ($e) {
-		$hash->{$e} = $self->recursive_hash(\@array, $val);
+	my ($self, $aref, $val)  = @_,3;
+	my $element = shift(@{$aref});
+	my $hash = {};
+	if ($element) {
+		$hash->{$element} = $self->recursive_hash($aref, $val);
 		return $hash;
 	}
 
+	# strip quotes off quotes if found - slight speed up
 	if (substr($val, 0, 1) eq "\"") {
 		$val =~ s/"//g;
 		return $val;
