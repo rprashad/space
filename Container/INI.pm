4f51161e4981e4a1fb405189c7fa7a18fff330a2
Raj Prashad <rprashad@cheetahmail.com>
2013-04-18 15:47:38 -0400

    Added multiple context separators '::' and cleaned up more cruft
    [oncall::us::ny]
    tier1.support=J Smith, J Doe
    tier1.contact=212-555-1212,212-555-1213
    tier2.support=P Rabbit
    tier2.contact=212-555-1214
    address.street=1 Wall St
    address.city=New York
    address.state=New York
    address.zip=10006
    
    [disk::quotas]
    var=20G
    usr=40G
    home=100G
    
    $VAR1 = {
              'disk' => {
                          'quotas' => {
                                        'var' => '20G',
                                        'home' => '100G',
                                        'usr' => '40G'
                                      }
                        },
              'oncall' => {
                            'us' => {
                                      'ny' => {
                                                'address' => {
                                                               'zip' => '10006',
                                                               'city' => 'New York',
                                                               'street' => '1 Wall St',
                                                               'state' => 'New York'
                                                             },
                                                'tier2' => {
                                                             'support' => 'P Rabbit',
                                                             'contact' => '212-555-1214'
                                                           },
                                                'tier1' => {
                                                             'support' => [
                                                                            'J Smith',
                                                                            'J Doe'
                                                                          ],
                                                             'contact' => [
                                                                            '212-555-1212',
                                                                            '212-555-1213'
                                                                          ]
                                                           }
                                              }
                                    }
                          }
            };

--- Container/INI.pm
+++ Container/INI.pm
@@ -12,7 +12,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 use strict;
-use warnings;
 package Container::INI;
 
 sub new {
@@ -26,117 +25,109 @@ sub new {
 sub source_config {
   my $self = $_[0];
 
-  open(CFG, "< $self->{file}" ) or die __PACKAGE__ . "Error reading ini: $self->{file}\n";
+  open(CFG, "< $self->{file}" ) or die __PACKAGE__ . " Error reading ini: $self->{file}\n";
   my ($module, $hpoint, $key, $value, $evalue, $continue, $quoted);
-
+  $module = {};
   while(<CFG>) {
    chomp($_);
    # ignore lines that begin with '#' or ';' or whitespace
-   if ($_ !~ /^\s*?#/ && $_ ne "" && $_ !~ /^;/ or $quoted > 0) {
+   if ($_ !~ /^\s*?(?:#|;)/) {
      # Find context/header
-     	if ($_ =~ /^\s*\[\s*(\w+)?:?:?(\w+)?\s*\]/) {
-	  my $mod = $1;
-	  my $options = $2;
-	  $hpoint = undef;
-
-	  if (!defined($module->{$mod})) {
-     	    $module->{$mod} = undef;
-	    $hpoint = \$module->{$mod};
-	  }
-	  if ($options) {
-	    $hpoint = \$module->{$mod}->{$options};
-	  }
-	 else {
-	  # context already exists
-	  $hpoint = \$module->{$mod};
-	  }
+     	if ($_ =~ /\[\s*(.*)\s*\]/g) {
+		# this allows for unlimited context separators '::' to be present in the header
+		# [this::is::valid] = $this->{is}->{valid}
+		my $conf = $self->build_conf([ split /[:]{2}/, $1 || ( $1 ) ], undef);
+		$module = $self->merge_conf($module, $conf);
+		$hpoint = $self->build_ref_point($module, $conf);
        } # end context/header
 		
 	# process key/value pairs
-	else {
-	  # line continuation code
-	  if (!$continue and !$quoted) {
-	    ($key, $value) = split("=",$_,2);
-	    $key =~ s/\s+//g;
-	  }
-	  else {
-	   $value = $_;
-	  }
-			
-	  # found line continuation char \
-	  if ($value =~ /(.*)\s*?\\\s*?$/) {
-	    $continue += 1;
-	  }
+        else {
+          # line continuation code
+          if (!$continue and !$quoted) {
+            ($key, $value) = split("=",$_,2);
+            $key =~ s/\s+//g;
+          }
+          else {
+           $value = $_;
+          }
 
-	  # found quoted text
-	  elsif ($value =~ /"/ && $value !~ /"[^"]+"/) {
-	    $quoted++;
-	  }
+          # found line continuation char \
+          if ($value =~ /(.*)\s*?\\\s*?$/) {
+            $continue += 1;
+          }
 
-	  # single line entity - build/merge
-	  if (!$continue and !$quoted) {
-	    my $conf = $self->build_conf([ split /\./, $key ], $value);
-	    $$hpoint = $self->merge_conf($$hpoint, $conf);
-	  }
-			
-	  # found line continuation char \	
-	  elsif ($value =~ /(.*)?\s*?\\\s*?$/) {
-	    $value = $1 . "\n";
-	    $evalue .= $value;
-	  }
-	  # found a quote
-	  elsif ($quoted == 1) {
-	    $evalue .= "$value\n";
-	  }
-	  # we've reached the end of our multi-line value
-	  else {
-	    $evalue .= $value;
-	    my $conf = $self->build_conf([ split /\./, $key ], $evalue);
-	    $$hpoint = $self->merge_conf($$hpoint, $conf);
-	    $continue = $evalue = $quoted = undef;
-	  }
+          # found quoted text
+          elsif ($value =~ /"/ && $value !~ /"[^"]+"/) {
+            $quoted++;
+          }
+
+          # single line entity - build/merge
+          if (!$continue and !$quoted) {
+            my $conf = $self->build_conf([ split /\./, $key ], $value);
+            $$hpoint = $self->merge_conf($$hpoint, $conf);
+          }
+
+          # found line continuation char \      
+          elsif ($value =~ /(.*)?\s*?\\\s*?$/) {
+            $value = $1 . "\n";
+            $evalue .= $value;
+          }
+          # found a quote
+          elsif ($quoted == 1) {
+            $evalue .= "$value\n";
+          }
+          # we've reached the end of our multi-line value
+          else {
+            $evalue .= $value;
+            my $conf = $self->build_conf([ split /\./, $key ], $evalue);
+            $$hpoint = $self->merge_conf($$hpoint, $conf);
+            $continue = $evalue = $quoted = undef;
+          }
        } # end key/val procesing
   } # end syntax check
  } # end while
 
   close(CFG);
   $self->{conf} = $module;
-  return \$self->{conf};
+  return $self->{conf};
 }
 
 sub get_config() {
   return $_[0]->{conf};
 }
 
+# merges two hash references 
 sub merge_conf {
   my ($self, $dst, $src) = @_,;
-  my $return =  { %{ $dst || {} } }; # shallow copy (if undef will give an new empty hash
+  my $return =  { %{ $dst || {} } };
   
   foreach my $key (keys %{$src}) {
     my $ref = ref($src->{$key});
     if ($ref eq 'HASH') {
       if( defined $return->{$key} && !ref( $return->{$key} ) ) {
-        die __PACKAGE__ . "previous scalar assignment detected for key '$key', cannot reassign as ref";
+        warn __PACKAGE__ . " found previous assignment for '$key', reassigning";
       }
       $return->{$key} = $self->merge_conf($return->{$key}, $src->{$key})
     } 
     elsif ($ref eq 'ARRAY') {
-      $return->{$key} = []; # initialize incase it's not an array from the globals
+      $return->{$key} = []; 
       @{$return->{$key}} = @{$src->{$key}};
     } 
     else {
       if( defined $return->{$key} && ref( $return->{$key} ) ){
-        die __PACKAGE__ . "previous ref assignment detected for key '$key', cannot reassign as scalar";
+         warn __PACKAGE__ . " found previous assignment for '$key', reassigning";
       }
       $return->{$key} = $src->{$key};
     }
   }
 return $return;
 
-} 
+}
 
+# converts an array in index order to a nested hash if applicable
 sub build_conf {
-  my ($self, $aref, $val)  = @_,;
+  my ($self, $aref, $val)  = @_;
   my $element = shift(@{$aref});
   my $hash = {};
   if ($element) {
@@ -144,13 +135,24 @@ sub build_conf {
     return $hash;
   }
 
-  # strip quotes off quotes if found - slight speed up
-  if (substr($val, 0, 1) eq "\"") {
-    $val =~ s/"//g;
-    return $val;
+    my (@a) = map { s/\\,/,/g; s/(?<!\\)"//g; $_ } split(/\s*(?<!\\),\s*/,$val);
+    (scalar(@a) > 1) ? return \@a : return $a[0];
+}
+
+# return a ref to what would be or is the value
+sub build_ref_point {
+  my ($self, $module, $conf)  = @_;
+  foreach my $key (keys %{$conf}) {	
+    if (ref($module->{$key})) {
+      return $self->build_ref_point($module->{$key}, $conf->{$key});
+    }
+    else {
+	if (!defined($module->{$key})) {
+		$module->{$key} = undef;
+	}
+	return \$module->{$key};
+	}
   }
-  my (@a) = map { s/\\,/,/g; $_ } split(/\s*(?<!\\),\s*/,$val);
-  (scalar(@a) > 1) ? return \@a : return $a[0];
 }
 
 1;
