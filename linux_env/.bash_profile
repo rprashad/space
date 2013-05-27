a8aa456912115a48c176ae58962ed74784e21a82
Nicholas R. Prashad <nickp@mint>
2013-05-27 12:35:29 -0400

    env settings

---
+++ linux_env/.bash_profile
@@ -0,0 +1,13 @@
+alias starfish='ssh starfish'
+
+PATH=$PATH:/home/nickp/bin:/home/nickp/bin/nextchange:/usr/bin:/usr/sbin:/usr/local/bin:/bin:/sbin:/home/nickp/dev/go/bin
+export GOROOT=/home/nickp/dev/go/
+export GOOS=linux
+export GOARCH=amd64
+export GOBIN=/home/nickp/dev/go/bin
+
+export PERL5LIB=/data/ops/lib/:$HOME/space/perl
+export PYTHONPATH=$HOME/space/python
+
+$HOME/bin/goscreen.pl
+
---
+++ linux_env/goscreen.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+# look for Attached and Detached screen sessions
+my @screens = split(/\n/, `screen  -ls | grep 'tached'`);
+
+# loop through list and prompt to attach or create a new screen unless
+# TERM=screen :)
+#
+if (scalar(@screens) > 0) {
+  for ($x=0; $x<= $#screens; $x++) {
+    my $y = $x+1;
+    print "$y) $screens[$x]\n";
+  }
+
+  if ($ENV{'TERM'} ne 'screen') {
+    print "(New=n, Existing=#): ";
+    my $choice = <STDIN>; chomp($choice);
+    if ($choice =~ /n/i) {
+      print "session name: ";
+      $name = <STDIN>; chomp($name);
+      `screen -S $name`;
+    }
+    else {
+      if ($choice =~ /\d/) {
+        my $session = (split(/\s+/, $screens[$choice - 1]))[1];
+        if ($screens[$choice - 1] =~ /Detached/) {
+          print "Reattaching Screen: $session ($choice)\n";
+          `screen -r $session`;
+        }
+        else {
+          print "Stealing session: $session\n";
+          `screen -dRR $session`;
+        }
+      }
+    }
+  }
+}
