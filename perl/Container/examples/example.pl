c0663dadc9226758698a74343ed4c09f5b36295b
Rajendra Prashad <raj@prashad.net>
2013-11-02 18:24:36 -0400

    Adding a pretty helpful by Niko

--- perl/Container/examples/example.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-use Container::INI;
-use Data::Dumper;
-
-print Dumper [  
-	Container::INI->new("example1.ini")->get_config ,
-        Container::INI->new("example2.ini")->get_config 
-      ];
