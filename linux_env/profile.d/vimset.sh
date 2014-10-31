bb60fcfd1832fabb3ebfebb07556c8b6e6d2d5a5
Raj Prashad <raj@prashad.net>
2014-10-31 09:32:52 -0400

    adding some helper tools to vim

--- linux_env/profile.d/vimset.sh
+++ linux_env/profile.d/vimset.sh
@@ -9,3 +9,16 @@ if [[ ! -d "$HOME/.vim/bundle/nerdtree" ]]; then
   cd $HOME/.vim/bundle
   git clone https://github.com/scrooloose/nerdtree.git
 fi
+
+if [[ ! -d "$HOME/.vim/bundle/vimrails" ]]; then
+  echo "Adding VIMRails"
+  cd $HOME/.vim/bundle
+  git clone git://github.com/tpope/vim-rails.git
+  git clone git://github.com/tpope/vim-bundler.git
+fi
+
+if [[ ! -d "$HOME/.vim/bundle/vim-ruby" ]]; then
+  echo "Adding VIMRails"
+  cd $HOME/.vim/bundle
+  git clone git://github.com/vim-ruby/vim-ruby.git
+fi
