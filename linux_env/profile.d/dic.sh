7dbcb12fda41345ef8b8ad8f8bbc1365a8e3fd7d
Rajendra Prashad <raj@prashad.net>
2016-09-07 22:32:46 -0400

    updating

--- linux_env/profile.d/dic.sh
+++ linux_env/profile.d/dic.sh
@@ -2,8 +2,8 @@ dic="/usr/share/stardict/dic/"
 
 if [[ ! -d "$dic" ]]; then
   echo "Making dictionary directory: $dic"
-  sudo mkdir -p /usr/share/stardict/dic/
-  sudo rsync -av $HOME/git/space/linux_env/dictionary/ /usr/share/stardict/dic/
+  sudo mkdir -p /usr/local/share/stardict/dic/
+  sudo rsync -av $HOME/git/space/linux_env/dictionary/ /usr/local/share/stardict/dic/
   echo "Dictionary Added, please install sdcv"
 fi
 
