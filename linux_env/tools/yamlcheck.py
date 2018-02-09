e117772546caebf62a73c0dce5cf304aace75996
Rajendra Prashad <rprashad@cloud-node2.prashad.net>
2018-02-09 20:42:00 +0000

    most default envs now are python3 - updating

--- linux_env/tools/yamlcheck.py
+++ linux_env/tools/yamlcheck.py
@@ -5,7 +5,7 @@ import yaml, sys, os.path
 arglen = len(sys.argv)
 if arglen == 2 and os.path.isfile(sys.argv[1]):
   try:
-    print yaml.load(open(sys.argv[1], 'r')) 
+    print(yaml.load(open(sys.argv[1], 'r')))
   except Exception as e:
-    print "Error %s" % e
+    print("Error %s" % e)
 
